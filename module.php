d3a24dca89cc1da471072e2fbff32e8f2a26fe81
LEsposito <loris.esposito@laposte.net>
2018-11-18 21:37:10 +0100

    Started to code module page with files, added file class

--- module.php
+++ module.php
@@ -8,7 +8,18 @@ $p = new BootstrapPage("Enseignements");
 $pdo = myPDO::getInstance();
 
 $p->appendContent(Layout::nav());
-//$mod = Module::getModuleById($_POST['idMod']);
+$mod = Module::getModuleById($_GET['id']);
 
+$title = "<div class='container container-mod'> \n <h1 style='text-align : center'>" . $mod->getLibModule() . "</h1>";
+
+$cmPart = <<<HTML
+
+  <div class = "cmPart">
+    <h2 style ="text-align : left"> Cours magistraux </h2>
+    <hr id="hrMod"/>
+
+HTML;
+
+$p->appendContent($title);
+$p->appendContent($cmPart);
 echo $p->toHTML();
-
---
+++ php/fichier.class.php
@@ -0,0 +1,10 @@
+<?php
+include_once("autoload.include.php");
+
+class Fichier{
+
+  protected $idFichier;
+  protected $nomFichier;
+  protected $descFichier;
+  protected $typeFichier;
+}
